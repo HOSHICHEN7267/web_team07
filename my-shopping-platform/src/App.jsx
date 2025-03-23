c1a621e31fb77f5c37cc86cebf1e4277ca34c8ac
poyucheese <linoscar92@gmail.com>
2025-03-23 19:12:55 +0800

    Add Homepage

--- my-shopping-platform/src/App.jsx
+++ my-shopping-platform/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
@@ -7,9 +8,10 @@ function App() {
     <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-[#4A90E2] to-[#50E3C2]">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </div>
