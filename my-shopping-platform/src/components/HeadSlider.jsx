48c873eacf6a7fc19ac532fa5aafa72cc41e5d95
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-03-24 22:24:42 +0800

    RWD: HeadSlider

--- my-shopping-platform/src/components/HeadSlider.jsx
+++ my-shopping-platform/src/components/HeadSlider.jsx
@@ -56,16 +56,15 @@ const HeaderSlider = () => {
           >
             <div className="md:pl-8 mt-10 md:mt-0">
               <p className="md:text-base text-orange-600 pb-1">{slide.offer}</p>
-              <h1 className="max-w-lg md:text-[40px] md:leading-[48px] text-2xl font-semibold">
+              <h1 className="max-w-lg md:text-[40px] text-2xl font-semibold md:leading-[56px] leading-[40px]">
                 {slide.title}
               </h1>
-              <div className="flex items-center mt-4 md:mt-6 ">
+              <div className="flex items-center mt-4 md:mt-6">
                 <button className="md:px-10 px-7 md:py-2.5 py-2 bg-orange-600 rounded-full text-white font-medium">
                   {slide.buttonText1}
                 </button>
                 <button className="group flex items-center gap-2 px-6 py-2.5 font-medium">
                   {slide.buttonText2}
-                  {/* <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon} alt="arrow_icon" /> */}
                 </button>
               </div>
             </div>
