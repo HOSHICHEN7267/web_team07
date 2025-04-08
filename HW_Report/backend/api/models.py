0ab724a76ea3f2ae7dabfcf5a5175861b38c3774
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-04-08 18:40:37 +0800

    Add user class and login/register url

--- HW_Report/backend/api/models.py
+++ HW_Report/backend/api/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 class Product(models.Model):
+    pid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     img = models.CharField(max_length=255)  # 存放圖片路徑 "/img/items/xxx.jpg"
 
     def __str__(self):
         return self.name
+    
+
+class User(models.Model):
+    uid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
