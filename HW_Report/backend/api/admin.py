c489fef0271f4edec97e48e83535b4bfa0091944
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-04-08 20:56:56 +0800

    Hide Django default user field (First name, Last name)

--- HW_Report/backend/api/admin.py
+++ HW_Report/backend/api/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from .models import Product
 
 admin.site.register(Product)
+
+class CustomUserAdmin(DefaultUserAdmin):
+    list_display = ('username', 'email', 'is_staff')  # ✅ 只顯示你想要的欄位
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
