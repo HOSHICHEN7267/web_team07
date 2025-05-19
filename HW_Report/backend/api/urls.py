41132ccb4997ab3ad5a0649952c03fbe2afbf4e7
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-05-19 23:22:59 +0800

    fix: Product detail api route typo

--- HW_Report/backend/api/urls.py
+++ HW_Report/backend/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('', views.index),
     # path('login/', views.login),
     path('products/', views.get_products),
-    path('api/products/<int:id>/', views.get_product_detail, name='get_product_detail'),  # 0519: Add product detail view
+    path('products/<int:id>/', views.get_product_detail, name='get_product_detail'),  # 0519: Add product detail view
     path('register/', views.register_user),  # ✅ 註冊用 API
     path('become-seller/', views.become_seller),
     path('ajax-test/', views.ajax_test_view),
