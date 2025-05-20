bd8ded72a8068493a42c3d10a3659cd8e4085231
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-05-20 14:38:16 +0800

    Fix static file path, add product fields, and update docker volume

--- HW_Report/backend/my_backend/urls.py
+++ HW_Report/backend/my_backend/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from django.conf import settings
+from django.conf.urls.static import static  # 🔽 加入這行
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# 🔽 加上這段：只有 DEBUG=True 時才會啟用，開發用
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
